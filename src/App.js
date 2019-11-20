e841dbb99b31eb1b6e3b203d152c35b469993a84
Mike <mgbutts@gmail.com>
2019-11-19 19:48:19 -0500

    setup users/useitems using public api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { users: [], loading: false };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Users users={this.state.users} loading={this.state.loading} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const UserItem = props => {
+  const { login, avatar_url, html_url } = props.user;
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        className="round-img"
+        alt=""
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} target="_blank" className="btn btn-dark btn-sm my-1">
+          More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default UserItem;
