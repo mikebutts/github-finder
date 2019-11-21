8aae9dd957c663805223abba15a196e024409338
Mike <mgbutts@gmail.com>
2019-11-21 00:36:20 -0500

    finished applicastion, refactoring for hooks

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ import Alert from "./components/layout/Alert";
 import "./App.css";
 import axios from "axios";
 import About from "./components/pages/About";
+import User from "./components/users/User";
 
 class App extends Component {
-  state = { users: [], loading: false, alert: null };
+  state = { users: [], loading: false, alert: null, user: {}, repos: [] };
 
   async componentDidMount() {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
     );
     this.setState({ users: res.data, loading: false });
   }
@@ -30,17 +31,34 @@ class App extends Component {
   searchUsers = async text => {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
     );
     this.setState({ users: res.data.items, loading: false });
   };
+
+  getUser = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?&client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
+    );
+    this.setState({ user: res.data, loading: false });
+  };
+  //GEt users repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
+    );
+    this.setState({ repos: res.data, loading: false });
+  };
   render() {
+    const { users, user, loading, alert, repos } = this.state;
     return (
       <Router>
         <div className="App">
           <Navbar />
           <div className="container">
-            <Alert alert={this.state.alert} />
+            <Alert alert={alert} />
             <Switch>
               <Route
                 exact
@@ -50,17 +68,28 @@ class App extends Component {
                     <Search
                       searchUsers={this.searchUsers}
                       clearUsers={this.clearUsers}
-                      showClear={this.state.users.length > 0 ? true : false}
+                      showClear={users.length > 0 ? true : false}
                       setAlert={this.setAlert}
                     />
-                    <Users
-                      users={this.state.users}
-                      loading={this.state.loading}
-                    />
+                    <Users users={users} loading={loading} />
                   </Fragment>
                 )}
               />
               <Route exact path="/about" component={About} />
+              <Route
+                exact
+                path="/user/:login"
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
+                    repos={repos}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
